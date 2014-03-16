8338af6ba39a713917339bb53e4bddef4c6455d0
Lin-Chun-Yi <c776188_60113@yahoo.com.tw>
2014-03-16 12:33:08 +0800

    初步!
    已能執行.計算

--- src/hw_02/JFBMI.java
+++ src/hw_02/JFBMI.java
@@ -2,15 +2,17 @@ package hw_02;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLayeredPane;
-import javax.swing.JEditorPane;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.JButton;
 
 public class JFBMI extends JFrame {
 
@@ -23,7 +25,8 @@ public class JFBMI extends JFrame {
 	private JTextField textHeight;
 	private JTextField textWeight;
 	private JTextField textBmi;
-
+	JButton bt = new JButton("Calculate");
+	
 	/**
 	 * Launch the application.
 	 */
@@ -32,6 +35,10 @@ public class JFBMI extends JFrame {
 			public void run() {
 				try {
 					JFBMI frame = new JFBMI();
+					frame.pack();
+					frame.setTitle("BMI");
+				    frame.setLocationRelativeTo(null); // Center the frame
+				    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -55,45 +62,63 @@ public class JFBMI extends JFrame {
 		contentPane.add(layeredPane, BorderLayout.CENTER);
 		
 		JLabel lblHeight = new JLabel("Height");
-		lblHeight.setBounds(140, 117, 46, 15);
+		lblHeight.setBounds(140, 85, 46, 15);
 		layeredPane.add(lblHeight);
 		
 		JLabel lblAge = new JLabel("Age");
-		lblAge.setBounds(140, 68, 46, 15);
+		lblAge.setBounds(140, 37, 46, 15);
 		layeredPane.add(lblAge);
 		
 		JLabel lblWeight = new JLabel("Weight");
-		lblWeight.setBounds(140, 172, 46, 15);
+		lblWeight.setBounds(140, 129, 46, 15);
 		layeredPane.add(lblWeight);
 		
 		textAge = new JTextField();
 		textAge.setHorizontalAlignment(SwingConstants.RIGHT);
-		textAge.setBounds(34, 65, 96, 21);
+		textAge.setBounds(34, 34, 96, 21);
 		layeredPane.add(textAge);
 		textAge.setColumns(10);
-		int age = Integer.parseInt(textAge.getText());
 		
 		textHeight = new JTextField();
 		textHeight.setHorizontalAlignment(SwingConstants.RIGHT);
-		textHeight.setBounds(34, 114, 96, 21);
+		textHeight.setBounds(34, 82, 96, 21);
 		layeredPane.add(textHeight);
 		textHeight.setColumns(10);
-		double height = Double.parseDouble(textHeight.getText());
 		
 		textWeight = new JTextField();
 		textWeight.setHorizontalAlignment(SwingConstants.RIGHT);
-		textWeight.setBounds(34, 169, 96, 21);
+		textWeight.setBounds(34, 126, 96, 21);
 		layeredPane.add(textWeight);
 		textWeight.setColumns(10);
-		double weight = Double.parseDouble(textWeight.getText());
 		
-		BMI myBMI = new BMI("name", age, weight, height);
+		
 		textBmi = new JTextField();
 		textBmi.setHorizontalAlignment(SwingConstants.CENTER);
 		textBmi.setBounds(260, 140, 96, 21);
 		layeredPane.add(textBmi);
 		textBmi.setColumns(10);
 		
-		textBmi.setText(String.format("%.2f", myBMI.getBMI()));
+		bt.setBounds(43, 169, 87, 23);
+		layeredPane.add(bt);
+		
+		
+		
+		bt.addActionListener(new ButtonListener());
+	}
+	
+	class ButtonListener implements ActionListener {
+	    @Override 
+	    public void actionPerformed(ActionEvent e) {
+	    	// Get values from text fields
+
+	    	int age = Integer.parseInt(textAge.getText());
+	    	double weight = Double.parseDouble(textWeight.getText());
+	    	double height = Double.parseDouble(textHeight.getText());
+
+	    	// Create a  object
+	    	BMI myBMI = new BMI("name", age, weight, height);
+
+	    	textBmi.setText(String.format("%.2f", myBMI.getBMI()));
+	    }
 	}
 }
